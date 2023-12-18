ba24e33b0d3fe558421aa85a0aa8f918b1da8d5e
jiaxi001 <jiabiao_1234@126.com>
2023-12-18 16:04:28 +0800

    1

---
+++ src/main/java/com/jiaxi/springbootstudy/SpringbootStudyApplication.java
@@ -0,0 +1,29 @@
+package com.jiaxi.springbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//public class SpringbootStudyApplication extends SpringBootServletInitializer {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(SpringbootStudyApplication.class, args);
+//    }
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(SpringbootStudyApplication.class);
+//    }
+//
+//}
+
+@SpringBootApplication
+public class SpringbootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiaxi/springbootstudy/SpringbootStudyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jiaxi.springbootstudy;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
